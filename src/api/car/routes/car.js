fc64c2fe090e8557bb517f89961ef556e1d1f37e
Jibril Mohamod <jazimjibril@gmail.com>
2022-08-30 21:03:34 +0300

    jman

--- src/api/car/routes/car.js
+++ src/api/car/routes/car.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * category router.
+ * car router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::category.category');
+module.exports = createCoreRouter('api::car.car');
